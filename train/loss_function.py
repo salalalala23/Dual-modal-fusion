e60c45d594a0f00faa40951f222cd909040ea4b3
Xiaoyu Yi <44748561+salalalala23@users.noreply.github.com>
2024-09-24 10:20:39 +0800

    Update loss_function.py

--- train/loss_function.py
+++ train/loss_function.py
@@ -59,20 +59,7 @@ class qua_loss(nn.Module):
         for i in range(label.shape[0]):
             label[i][int(t[i])] = 1
         l = label.softmax(dim=-1)
-        # l4 = F.kl_div((out[:bs]+out[bs:2 * bs]).softmax(dim=-1).log(), l, reduction='batchmean')
-        # a = torch.add(out[:bs], out[bs:2 * bs])
-        # b = torch.add(out[2 * bs:3 * bs], out[3 * bs:])
-        l4 = F.kl_div((p+q).softmax(dim=-1).log(), l, reduction='batchmean')#+\
-        # smoothl1(a, label) # + \
-        # F.kl_div(q.log(), l, reduction='batchmean') #+ \
-        # F.kl_div(r.log(), l, reduction='batchmean') + \
-        # F.kl_div(s.log(), l, reduction='batchmean')
-        # print(l4.item(), torch.isnan(P), torch.isnan(Q), torch.isnan(l))
-        # l4 = F.kl_div(r.log(), l, reduction='batchmean')
-        # criteron = nn.CrossEntropyLoss()
-        # l4 = criteron(out, t.long())
-        # return l_class + l4
-        # IOU = (KL_M_P * KL_P_M) / (KL_M_GP * KL_P_GM)
+        l4 = F.kl_div((p+q).softmax(dim=-1).log(), l, reduction='batchmean')
         return l4
 
 
@@ -96,49 +83,6 @@ class qua_loss(nn.Module):
 
         loss = alpha * (l1 + l2) + beta * l3 + gamma * l4
         return loss
-
-
-def KL_loss(p, q, r, s, t):
-    criteron = nn.CrossEntropyLoss()
-    MSE = nn.MSELoss()
-    a = torch.add(p, q)
-    b = torch.add(r, s)
-    tao = 3e-4
-    KL_M_P = F.kl_div(q.softmax(dim=-1).log(), p.softmax(dim=-1), reduction='batchmean')
-    KL_M_GM = F.kl_div(r.softmax(dim=-1).log(), p.softmax(dim=-1), reduction='batchmean')
-    KL_M_GP = F.kl_div(s.softmax(dim=-1).log(), p.softmax(dim=-1), reduction='batchmean')
-    KL_P_M = F.kl_div(p.softmax(dim=-1).log(), q.softmax(dim=-1), reduction='batchmean')
-    KL_P_GP = F.kl_div(s.softmax(dim=-1).log(), q.softmax(dim=-1), reduction='batchmean')
-    KL_P_GM = F.kl_div(r.softmax(dim=-1).log(), q.softmax(dim=-1), reduction='batchmean')
-    # print(KL_M_P, KL_M_GM, KL_M_GP, KL_P_M, KL_P_GP, KL_P_GM)
-    # l1 = KL_M_P + KL_M_GM + torch.pow(KL_M_GP-KL_M_GM+tao, 2)
-    # l2 = KL_P_M + KL_P_GP + torch.pow(KL_P_GM-KL_P_GP+tao, 2)
-    l1 = torch.pow(KL_M_P-KL_M_GM, 2) + KL_M_GM + torch.pow(KL_M_GP-KL_M_GM+tao, 2)
-    l2 = torch.pow(KL_P_M-KL_P_GP, 2) + KL_P_GP + torch.pow(KL_P_GM-KL_P_GP+tao, 2)
-    l3 = KL_M_GP / p + KL_P_GM / q
-    # print(l1, l2, l3)
-    # l1 = torch.exp(-torch.clip(KL_M_P + KL_M_GM - Kl_M_GP, min=0))
-    # l2 = torch.exp(-torch.clip(KL_P_M + KL_P_GP - KL_P_GM, min=0))
-    # l3 = torch.exp(-torch.abs(Kl_M_GP / p)) + torch.exp(-torch.abs(KL_P_GM / q))
-    label = torch.zeros(p.shape).to('cuda')
-    for i in range(label.shape[0]):
-        label[i][int(t[i])] = 1
-    # print(p.shape, p.softmax(dim=-1).shape, p.softmax(dim=-1).log().shape)
-    # print(p, p.softmax(dim=-1), p.softmax(dim=-1).log())
-    # print(t.shape, t.softmax(dim=-1).shape, t.softmax(dim=-1).log().shape)
-    l4 = F.kl_div(p.softmax(dim=-1).log(), label.softmax(dim=-1), reduction='batchmean') + \
-         F.kl_div(q.softmax(dim=-1).log(), label.softmax(dim=-1), reduction='batchmean') #+ \
-         # F.kl_div(r.softmax(dim=-1).log(), label.softmax(dim=-1), reduction='batchmean') + \
-         # F.kl_div(s.softmax(dim=-1).log(), label.softmax(dim=-1), reduction='batchmean')# + MSE(a, label) + MSE(b, label)
-    # l_class = criteron(a, t.long())
-    # return l_class + l4
-    IOU = (KL_M_P*KL_P_M) / (KL_M_GP*KL_P_GM)
-    return l1 + l2 + l3 + l4
-    # return torch.mean(l1 + l2 + l3 + l4)
-
-
-def test():
-    print(KL_loss(torch.randn(40, 8), torch.randn(20, 8), torch.randn(20, 8), torch.randn(20, 8)))
 
 
 # test()
